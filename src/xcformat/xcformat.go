24d481ddf2c2db081bd79d17e0451e8e337c0d2f
hezongzhu <hezongzhu@gmail.com>
2019-08-19 12:14:37 +0800

    rename file name

--- src/xcformat/xcformat.go
+++ src/xcformat/xcformat.go
@@ -2,7 +2,10 @@ package main
 
 import (
 	"bufio"
+	"flag"
+	"fmt"
 	"io"
+	"io/ioutil"
 	"log"
 	"os"
 	"os/exec"
@@ -14,8 +17,9 @@ import (
 )
 
 var (
-	formatRecord = map[string]int64{}
-
+	help            = false
+	debug           = false
+	formatRecord    = map[string]int64{}
 	whiteListExtMao = map[string]bool{
 		".h":     true,
 		".m":     true,
@@ -23,6 +27,27 @@ var (
 		".swift": true,
 	}
 )
+
+func usage() {
+	_, _ = fmt.Fprintf(os.Stderr, `xcformat version: xcformat/1.0
+Usage: xcformat [-d]
+Options:
+`)
+	flag.PrintDefaults()
+}
+
+func init() {
+	flag.BoolVar(&help, "h", false, "this help")
+	flag.BoolVar(&debug, "d", true, "show debug info")
+	// 改变默认的 Usage，flag包中的Usage 其实是一个函数类型。这里是覆盖默认函数实现，具体见后面Usage部分的分析
+	flag.Usage = usage
+}
+
+func debugPrintln(v ...interface{}) {
+	if debug {
+		log.Println("debug[xcformat]: ", v)
+	}
+}
 
 func ext(path string) string {
 	for i := len(path) - 1; i >= 0 && path[i] != '/'; i-- {
@@ -48,7 +73,15 @@ func isFileExist(path string) bool {
 	return true
 }
 
+func WriteWithIOutil(name string, content []byte) {
+	err := ioutil.WriteFile(name, content, 0644)
+	if err != nil {
+		log.Println(err)
+	}
+}
+
 func formatSwiftFile(path string) {
+	debugPrintln(path)
 	if size, ok := formatRecord[path]; ok {
 		info, err := os.Stat(path)
 		if err != nil {
@@ -56,26 +89,47 @@ func formatSwiftFile(path string) {
 			return
 		}
 		if size == info.Size() {
+			debugPrintln("both sides are the same, return!")
 			return
 		}
+		debugPrintln("size:", info.Size())
 	}
 
-	_, err := exec.Command("swiftformat", path).Output()
+	debugPrintln("format start...")
+
+	output, err := exec.Command("cat", path).Output()
 	if err != nil {
 		log.Println(err)
 		return
 	}
+
+	formatCommand := exec.Command("swiftformat")
+	stdin, err := formatCommand.StdinPipe()
+	io.WriteString(stdin, string(output))
+	stdin.Close()
+
+	out, err := formatCommand.CombinedOutput()
+	if err != nil {
+		log.Println(err)
+	}
+
+	debugPrintln("write to file...")
+	WriteWithIOutil(path, out)
+
+	debugPrintln("format over !!!")
 
 	info, err := os.Stat(path)
 	if err != nil {
 		log.Println(err)
 		return
 	}
+
 	formatRecord[path] = info.Size()
+	debugPrintln("format size :", info.Size())
 }
 
 func formatObjcFile(path string) {
-
+	debugPrintln(path)
 	if size, ok := formatRecord[path]; ok {
 		info, err := os.Stat(path)
 		if err != nil {
@@ -83,22 +137,33 @@ func formatObjcFile(path string) {
 			return
 		}
 		if size == info.Size() {
+			debugPrintln("both sides are the same, return!")
 			return
 		}
+		debugPrintln("size:", info.Size())
 	}
 
-	_, err := exec.Command("clang-format", "-i", path).Output()
+	//_, err := exec.Command("clang-format", "-i", path).Output()
+	debugPrintln("format start...")
+
+	byte, err := exec.Command("clang-format", path).Output()
 	if err != nil {
 		log.Println(err)
 		return
 	}
+	debugPrintln("write to file...")
+	WriteWithIOutil(path, byte)
+
+	debugPrintln("format over !!!")
 
 	info, err := os.Stat(path)
 	if err != nil {
 		log.Println(err)
 		return
 	}
+
 	formatRecord[path] = info.Size()
+	debugPrintln("format size :", info.Size())
 }
 
 func hasCommand(name string) bool {
@@ -109,7 +174,7 @@ func hasCommand(name string) bool {
 	return true
 }
 
-func execCommand(commandName string, params []string, handle func(string) ()) bool {
+func execCommand(commandName string, params []string, handle func(string)) bool {
 	//函数返回一个*Cmd，用于使用给出的参数执行name指定的程序
 	cmd := exec.Command(commandName, params...)
 
@@ -145,8 +210,6 @@ func execCommand(commandName string, params []string, handle func(string) ()) bo
 
 func handleFile(line string) {
 
-	log.Println(line)
-
 	// 如是目录或创建、删除操作
 	if strings.Contains(line, "Created") ||
 		strings.Contains(line, "Removed") ||
@@ -178,6 +241,7 @@ func handleFile(line string) {
 		fileName := fileInfo.Name()
 
 		if strings.Contains(fileName, "~.") {
+			debugPrintln(fileName, " is cache file.")
 			return
 		}
 
@@ -197,6 +261,11 @@ func handleFile(line string) {
 }
 
 func main() {
+	flag.Parse()
+	if help {
+		flag.Usage()
+		return
+	}
 
 	sigs := make(chan os.Signal, 1)
 
@@ -228,13 +297,16 @@ func main() {
 	pwd, _ := exec.Command("pwd").Output()
 	rootDir := strings.TrimSpace(string(pwd))
 	styleFile := path.Join(rootDir, ".clang-format")
-
 	if !isFileExist(styleFile) {
 		log.Fatalf(" Error: Cannot find %s \n", styleFile)
 		return
 	}
 
 	params := []string{"-xr", rootDir}
+
+	// test
+	//params := []string{"-xr", "/Users/apple/Documents/GitHub/PLTextView"}
+
 	execCommand("fswatch", params, handleFile)
 
 }
